2294f56a98738daa4f0eb8e2b1161966c957d021
Kiu <kiuushin@outlook.com>
2025-06-08 22:24:36 -0600

    Added new print statments for testing commits to github

--- pyscripts/myscript.py
+++ pyscripts/myscript.py
@@ -2,6 +2,9 @@
 
 def main():
     print("Hello, world!")
+    print("This is a simple Python script.") 
+    print("It can be executed directly or imported as a module.")
+    print("You can modify this script to suit your needs.")
 
 if __name__ == "__main__":
     main()
